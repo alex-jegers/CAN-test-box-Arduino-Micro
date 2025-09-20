f0b2d261dd2a0a1287af30ddd7c91ea4ac5e6fb6
Alex Jegers <jeger007@d.umn.edu>
2025-09-20 17:27:59 -0500

    Formatting and comments

--- examples/CAN_read/CAN_read.ino
+++ examples/CAN_read/CAN_read.ino
@@ -1,32 +1,50 @@
 #include <SPI.h>
 #include "mcp2515.h"
 
-struct can_frame canMsg;
+struct can_frame can_msg;
 MCP2515 mcp2515(10);
 
 
 void setup() {
   Serial.begin(115200);
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);
-  mcp2515.setNormalMode();
 
+  mcp2515.reset();
+
+  /*
+    This will set the baud rate. The second argument, "MCP_16MHZ",
+    tells the software what the speed of the physical clock on the board
+    is. Dont change that.
+  */
+  mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);  
+  //mcp2515.setBitrate(CAN_250KBPS, MCP_16MHZ);
+  //mcp2515.setBitrate(CAN_125KBPS, MCP_16MHZ);
+
+  /*
+    All configuration needs to happen before setting normal mode.
+    Default mode is config mode.
+  */
+  mcp2515.setNormalMode();  
 }
 
 void loop() {
   
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+  /*
+    This checks to see if there's new data in one of the buffers.
+    Will return ERROR_NOMSG if there's not.
+  */
+  if (mcp2515.readMessage(&can_msg) == MCP2515::ERROR_OK) {
+    /*Formats and prints CAN ID and data length code (DLC).*/
     Serial.print("CAN ID:");
-    Serial.print(canMsg.can_id, HEX); // print ID
-    Serial.print("  Length:"); 
-    Serial.print(canMsg.can_dlc, HEX); // print DLC
-    Serial.print("  Data: ");
-    
-    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
-      Serial.print(canMsg.data[i],HEX);
+    Serial.print(can_msg.can_id, HEX);
+    Serial.print("    Length:"); 
+    Serial.print(can_msg.can_dlc, HEX);
+    Serial.print("    Data: ");
+
+    /*Formats and prints all the data bytes.*/
+    for (int i = 0; i < can_msg.can_dlc; i++)  {
+      Serial.print(can_msg.data[i],HEX);
       Serial.print(" ");
     }
-
     Serial.println();      
   }
   
--- examples/CAN_write/CAN_write.ino
+++ examples/CAN_write/CAN_write.ino
@@ -1,46 +1,64 @@
 #include <SPI.h>
 #include "mcp2515.h"
 
-struct can_frame canMsg1;
-struct can_frame canMsg2;
+struct can_frame can_msg_1;
+struct can_frame can_msg_2;
 MCP2515 mcp2515(10);
 
 
 void setup() {
-  canMsg1.can_id  = 0x0F6;
-  canMsg1.can_dlc = 8;
-  canMsg1.data[0] = 0x8E;
-  canMsg1.data[1] = 0x87;
-  canMsg1.data[2] = 0x32;
-  canMsg1.data[3] = 0xFA;
-  canMsg1.data[4] = 0x26;
-  canMsg1.data[5] = 0x8E;
-  canMsg1.data[6] = 0xBE;
-  canMsg1.data[7] = 0x86;
+  /*
+    Here you can change the information contained in the CAN frame. ID is obv the identifier. DLC
+    is the Data Length Code and specifies how many bytes will be sent. The data array is the data.
+    You can add as many can_frame structs as you'd like.
+  */
+  can_msg_1.can_id  = 0x69;
+  can_msg_1.can_dlc = 8;
+  can_msg_1.data[0] = 0x8E;
+  can_msg_1.data[1] = 0x87;
+  can_msg_1.data[2] = 0x32;
+  can_msg_1.data[3] = 0xFA;
+  can_msg_1.data[4] = 0x26;
+  can_msg_1.data[5] = 0x8E;
+  can_msg_1.data[6] = 0xBE;
+  can_msg_1.data[7] = 0x86;
 
-  canMsg2.can_id  = 0xB00B;
-  canMsg2.can_dlc = 5;
-  canMsg2.data[0] = 0xDE;
-  canMsg2.data[1] = 0xAD;
-  canMsg2.data[2] = 0x00;
-  canMsg2.data[3] = 0xBE;
-  canMsg2.data[4] = 0xEF;
-  
-  while (!Serial);
-  Serial.begin(115200);
-  
+  can_msg_2.can_id  = 0xB00B;
+  can_msg_2.can_dlc = 5;
+  can_msg_2.data[0] = 0xDE;
+  can_msg_2.data[1] = 0xAD;
+  can_msg_2.data[2] = 0x00;
+  can_msg_2.data[3] = 0xBE;
+  can_msg_2.data[4] = 0xEF;
+
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);
+
+  /*
+    This will set the baud rate. The second argument, "MCP_16MHZ",
+    tells the software what the speed of the physical clock on the board
+    is. Dont change that.
+  */
+  mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);  
+  //mcp2515.setBitrate(CAN_250KBPS, MCP_16MHZ);
+  //mcp2515.setBitrate(CAN_125KBPS, MCP_16MHZ);
+
+  /*
+    All configuration needs to happen before setting normal mode.
+    Default mode is config mode.
+  */
   mcp2515.setNormalMode();
-  
-  Serial.println("Example: Write to CAN");
 }
 
 void loop() {
-  mcp2515.sendMessage(&canMsg1);
-  canMsg1.data[0]++;
+  mcp2515.sendMessage(&can_msg_1);
+  
+  /*
+    Changing some of the data in can_msg_1, this should be 
+    seen on the receiving end.
+  */
+  can_msg_1.data[0]++;            
   delay(500);
 
-  mcp2515.sendMessage(&canMsg2);
+  mcp2515.sendMessage(&can_msg_2);
   delay(500);
 }
